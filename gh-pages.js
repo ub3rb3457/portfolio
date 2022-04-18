09c26589d1facdca6309d11ea1c892a672552f86
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-04-18 01:00:57 -0400

    removing old files not needed due to github action to publish to gh-pages branch

--- gh-pages.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra')
-
-const source = './docs/.vitepress/dist'
-const dest = './gh-pages'
-
-fs.copySync(source, dest)
-// fs.moveSync(source, dest, { overwrite: true })
-
-console.log(source + ' files successfully moved to ' + dest)
