9497b8361a750c4e681ac4ec312a3d366809cc56
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-04-18 02:12:15 -0400

    tweaks to main content code examples

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: '/vite-vue-compo-pkg-starter/',
-    title: 'OBE:WDS ReplaceMe',
+    title: 'OBE:WDS',
     description: 'A documentation site for the ReplaceMe component',
     themeConfig: {
         nav: [
